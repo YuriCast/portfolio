5def5ad9cab18ba38e2a9a51dadad7931df13c01
Yuri Castilho <yuricastilho2005@gmail.com>
2023-10-13 15:34:20 -0300

    style: Intro section done

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,6 +4,10 @@ import Image from 'next/image'
 import React from 'react'
 import perfil from '../public/Perfil.jpg'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
+import { FaGithubSquare } from 'react-icons/fa'
 
 export default function Intro() {
   return (
@@ -25,8 +29,33 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         >
-            <span className='font-bold'>Olá, meu nome é Yuri</span>. Como <span className='font-bold'>front-end</span> há <span className='font-bold'>2 anos</span>, desenvolvi projetos utilizando de ferramentas atuais, código limpo e busca constante por aprimoramento.
+            <span className='font-bold'>Olá, meu nome é Yuri</span>. Como <span className='font-bold'>front-end</span> há <span className='font-bold'>2 anos</span>, desenvolvi projetos utilizando de ferramentas atuais e código limpo. Meu principal foco é <span className='font-bold'>React</span>
         </motion.p>
+
+        <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1, }}
+        >
+            <Link href='#contact'
+            className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+                Contate-me aqui <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+            </Link>
+
+            <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10' href='/CV.pdf' download>
+                Baixe meu CV <HiDownload className='opacity-60'/>
+            </a>
+
+            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 transition cursor-pointer border border-black/10'
+            href='https://www.linkedin.com/in/yuri-castilhoo/' target='_blank'>
+                <BsLinkedin />
+            </a>
+
+            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10'
+            href='https://github.com/YuriCast' target='_blank'>
+                <FaGithubSquare />
+            </a>
+        </motion.div>
     </section>
   )
 }
